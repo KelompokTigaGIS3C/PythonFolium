27d7e884ee783f40b7af174206f5704bc90648bb
mathovanidimas <mathovanidimas@gmail.com>
2017-10-29 15:13:26 +0700

    dimas commit

--- Bandung.py
+++ Bandung.py
@@ -1,20 +1,31 @@
 import folium
-
-k = folium.Map(
-    location=[-6.915188, 107.616022],
+m = folium.Map(
+    location=[-6.917418, 107.619465],
     zoom_start=12,
     tiles='Stamen Terrain'
 )
-
 tooltip = 'Click me!'
-    
-folium.Marker([-6.969774, 107.682983], popup='<i>Tegalluar Bojongsoang, Bandung, West Java</i>').add_to(k)
-folium.Marker([-6.966957, 107.686105], popup='<i>Derwati Rancasari, Bandung City, West Java</i>').add_to(k)
-folium.Marker([-6.968044, 107.688031], popup='<i>Rancabolang Gedebage, Bandung City, West Java</i>').add_to(k)
-folium.Marker([-6.903444, 107.554920], popup='<i>Cibeureum South Cimahi, Cimahi City, West Java</i>').add_to(k)
-folium.Marker([-6.903956, 107.658776], popup='<i>Cicaheum Kiaracondong, Bandung City, West Java</i>').add_to(k)
-folium.Marker([-6.847544, 107.536381], popup='<i>Cipageran North Cimahi, Cimahi City, West Java</i>').add_to(k)
-folium.Marker([-6.824876, 107.619980], popup='<i>Lembang West Bandung Regency, West Java</i>').add_to(k)
-folium.Marker([-6.822489, 107.551487], popup='<i>Jambudipa Cisarua, West Bandung Regency, West Java</i>').add_to(k)
-folium.Marker([-6.866462, 107.668732], popup='<i>Cimenyan Bandung, West Java</i>').add_to(k)
-folium.Marker([-6.911113, 107.711304], popup='<i>Pasanggrahan Ujung Berung, Bandung City, West Java</i>').add_to(k)
+folium.Marker([-6.871369, 107.573665], popup='<i>POLBAN</i>').add_to(m)
+folium.Marker([-6.873206, 107.575965], popup='<i>Politeknik Pos Indonesia</i>').add_to(m)
+folium.Marker([-6.878085, 107.573909], popup='<i>SMAN 15 Bandung</i>').add_to(m)
+folium.Marker([-6.879619, 107.579284], popup='<i>SMK Pasundan 5 Bandung</i>').add_to(m)
+folium.Marker([-6.885547, 107.580947], popup='<i>Universitas Kristen Maranatha</i>').add_to(m)
+folium.Marker([-6.889348, 107.603676], popup='<i>SMA Negeri 2 Bandung</i>').add_to(m)
+folium.Marker([-6.866352, 107.592781], popup='<i> Universitas Pasundan Bandung</i>').add_to(m)
+folium.Marker([-6.868355, 107.594583], popup='<i> Sekolah Tinggi Pariwisata Bandung</i>').add_to(m)
+folium.Marker([-6.861143, 107.592921], popup='<i> FPIPS Universitas Pendidikan Indonesia</i>').add_to(m)
+folium.Marker([-6.891496, 107.610654], popup='<i> Institut Teknologi Bandung</i>').add_to(m)
+folium.Marker([-6.874806, 107.605113], popup='<i> Universitas Katolik Parahyangan</i>').add_to(m)
+folium.Marker([-6.898046, 107.644692], popup='<i> Universitas Widyatama</i>').add_to(m)
+folium.Marker([-6.902606, 107.643690], popup='<i> Sekolah Menengah Atas Negeri 10 Bandung</i>').add_to(m)
+folium.Marker([-6.900007, 107.642628], popup='<i> STIMIK Bandung</i>').add_to(m)
+folium.Marker([-6.886823, 107.615287], popup='<i> Universitas Komputer Indonesia </i>').add_to(m)
+folium.Marker([-6.926047, 107.774602], popup='<i> Universitas Padjadjaran </i>').add_to(m)
+folium.Marker([-6.928475, 107.779475], popup='<i> SMA Negeri Jatinangor</i>').add_to(m)
+folium.Marker([-6.931271, 107.717319], popup='<i> Universitas Islam Negeri Sunan Gn. Djati</i>').add_to(m)
+ 
+folium.Marker([-6.903921, 107.607880], popup='<i> Universitas Islam Bandung </i>').add_to(m)
+folium.Marker([-6.905070, 107.608312], popup='<i> Universitas Pasundan Kampus 2</i>').add_to(m)
+
+m
+
