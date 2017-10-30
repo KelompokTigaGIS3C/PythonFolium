ec5623bc57dfde57de92624beb7d086f8e89be9d
mathovanidimas <mathovanidimas@gmail.com>
2017-10-30 15:38:13 +0700

    commit kedua, jumlah 30 koordinat

--- Bandung.py
+++ Bandung.py
@@ -15,6 +15,7 @@ folium.Marker([-6.866352, 107.592781], popup='<i> Universitas Pasundan Bandung</
 folium.Marker([-6.868355, 107.594583], popup='<i> Sekolah Tinggi Pariwisata Bandung</i>').add_to(m)
 folium.Marker([-6.861143, 107.592921], popup='<i> FPIPS Universitas Pendidikan Indonesia</i>').add_to(m)
 folium.Marker([-6.891496, 107.610654], popup='<i> Institut Teknologi Bandung</i>').add_to(m)
+
 folium.Marker([-6.874806, 107.605113], popup='<i> Universitas Katolik Parahyangan</i>').add_to(m)
 folium.Marker([-6.898046, 107.644692], popup='<i> Universitas Widyatama</i>').add_to(m)
 folium.Marker([-6.902606, 107.643690], popup='<i> Sekolah Menengah Atas Negeri 10 Bandung</i>').add_to(m)
@@ -25,6 +26,7 @@ folium.Marker([-6.928475, 107.779475], popup='<i> SMA Negeri Jatinangor</i>').ad
 folium.Marker([-6.931271, 107.717319], popup='<i> Universitas Islam Negeri Sunan Gn. Djati</i>').add_to(m)
 folium.Marker([-6.903921, 107.607880], popup='<i> Universitas Islam Bandung </i>').add_to(m)
 folium.Marker([-6.905070, 107.608312], popup='<i> Universitas Pasundan Kampus 2</i>').add_to(m)
+
 folium.Marker([-6.863278, 107.579878], popup='<i> Joglo Sentir Klasikan</i>').add_to(m)
 folium.Marker([-6.858751, 107.591631], popup='<i> Asrama Mahasiswa PPG Pasca SM-3T UPI</i>').add_to(m)
 folium.Marker([-6.860728, 107.593522], popup='<i> Gedung FPBS UPI</i>').add_to(m)
@@ -35,6 +37,7 @@ folium.Marker([-6.861224, 107.600126], popup='<i> TravelDay Bandung</i>').add_to
 folium.Marker([-6.885387, 107.580790], popup='<i> Gereja Jemaat Kristen Indonesia</i>').add_to(m)
 folium.Marker([-6.892407, 107.586461], popup='<i> Gereja Kristen Protestan Simalungun</i>').add_to(m)
 folium.Marker([-6.973988, 107.630407], popup='<i> Telkom University</i>').add_to(m)
+
 folium.Marker([-6.973314, 107.628806], popup='<i> Asrama Putri Telkom University</i>').add_to(m)
 folium.Marker([-6.971991, 107.631161], popup='<i> School of Creative Industries, Telkom University</i>').add_to(m)
 folium.Marker([-6.908559, 107.611097], popup='<i> Bandung Indah Plaza</i>').add_to(m)
@@ -45,6 +48,7 @@ folium.Marker([-6.912995, 107.609614], popup='<i> Taman Balai Kota Bandung</i>')
 folium.Marker([-6.914033, 107.610214], popup='<i> Taman Vanda</i>').add_to(m)
 folium.Marker([-6.898705, 107.612427], popup='<i> Taman Cikapundung</i>').add_to(m)
 folium.Marker([-6.909303, 107.615191], popup='<i> Taman Maluku</i>').add_to(m)
+
 folium.Marker([-6.898067, 107.609224], popup='<i> Taman Jomblo</i>').add_to(m)
 folium.Marker([-6.893640, 107.610678], popup='<i> Taman Ganesha</i>').add_to(m)
 folium.Marker([-6.894464, 107.608145], popup='<i> Museum Zoologi</i>').add_to(m)
@@ -55,8 +59,21 @@ folium.Marker([-6.893443, 107.612651], popup='<i> Warung Pasta Bandung</i>').add
 folium.Marker([-6.893280, 107.612914], popup='<i> Martabak San Francisco</i>').add_to(m)
 folium.Marker([-6.894977, 107.610949], popup='<i> Black Romantic</i>').add_to(m)
 folium.Marker([-6.879233, 107.620205], popup='<i> Daily Routine Coffe</i>').add_to(m)
+
 folium.Marker([-6.877100, 107.618113], popup='<i> Borma Dago</i>').add_to(m)
 folium.Marker([-6.877650, 107.616938], popup='<i> Lumbung Rasa</i>').add_to(m)
 
 
+folium.Marker([-6.912781, 107.664919], popup='<i> SMA Negeri 23 Bandung</i>').add_to(m)
+folium.Marker([-6.900972, 107.585742], popup='<i> SMAN 9 Bandung</i>').add_to(m)
+folium.Marker([-6.905546, 107.582892], popup='<i> SMA Angkasa</i>').add_to(m)
+folium.Marker([-6.906233, 107.592395], popup='<i> SMKN 12 Bandung</i>').add_to(m)
+folium.Marker([-6.890490, 107.558189], popup='<i> SMK 11 Bandung</i>').add_to(m)
+folium.Marker([-6.913189, 107.608553], popup='<i> Sekolah Menengah Kejuruan Negeri 1 Bandung</i>').add_to(m)
+folium.Marker([-6.895333, 107.612575], popup='<i> Sekolah Menengah Atas Negeri 1 Bandung</i>').add_to(m)
+folium.Marker([-6.891334, 107.603975], popup='<i> Sekolah Menengah Atas Pasundan 8</i>').add_to(m)
+folium.Marker([-6.873583, 107.616192], popup='<i> Sekolah Menengah Atas Negeri 19 Bandung</i>').add_to(m)
+folium.Marker([-6.904103, 107.622166], popup='<i> Sekolah Menengah Atas Negeri 20 Bandung</i>').add_to(m)
 
+
+m
